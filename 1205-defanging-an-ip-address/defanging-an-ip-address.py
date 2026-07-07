5f139dcede6e05d566f578bcbf9acef46d1045fd
Taqi Ul Hassan <taqihassan011@gmail.com>
2026-07-07 23:40:26 +0500

    Time: 10 ms (89.70%) | Memory: 12.4 MB (14.63%) - LeetSync

--- 1205-defanging-an-ip-address/defanging-an-ip-address.py
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.py
@@ -1,10 +1,4 @@
 class Solution(object):
     def defangIPaddr(self, address):
-        result = ""
-        for i in address:
-            if i == ".":
-                result += '[.]'
-            else:
-                result+=i
-        return result
+        return address.replace(".","[.]")
         
