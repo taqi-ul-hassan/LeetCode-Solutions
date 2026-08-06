00d8478ed6c04fc051a00a46f74ac56677d275dc
Taqi Ul Hassan <taqihassan011@gmail.com>
2026-08-06 18:14:08 +0500

    Time: 3 ms (45.78%) | Memory: 23.4 MB (80.30%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -1,5 +1,13 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        s.reverse()
-        print(s)
+        # s.reverse()
+        # print(s)
+        # SECOND WAY
+        stack = []
+        for ch in s:
+            stack.append(ch)
+        i = 0
+        while stack:
+            s[i] = stack.pop()
+            i+=1
         
